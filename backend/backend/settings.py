01bf774636c5b0edb613861b7b7fbf9f9de07289
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2023-06-13 10:49:24 +0630

    Allow Host to muliple

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qlnag^v2gvr2h+3f)ku7lgayno2q#tw4at8-v51&i_)njv!d=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
